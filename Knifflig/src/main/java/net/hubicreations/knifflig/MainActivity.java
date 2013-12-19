a71f8168c1762a1c653939086636d2aa0c8ed270
Hubi <constantine17@gmail.com>
2013-12-19 20:44:49 +0100

    added first test spinner

--- Knifflig/src/main/java/net/hubicreations/knifflig/MainActivity.java
+++ Knifflig/src/main/java/net/hubicreations/knifflig/MainActivity.java
@@ -3,6 +3,8 @@ package net.hubicreations.knifflig;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initSpinners();
     }
 
 
@@ -19,5 +23,15 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+
+    private void initSpinners() {
+        Spinner spinner = (Spinner) findViewById(R.id.OnesSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_values, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 }
+
